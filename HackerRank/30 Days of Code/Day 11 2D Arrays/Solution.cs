489387c14a7ea5f9a2571ce82be298a328fbef86
DaeunSim <49071747+DaeunSim@users.noreply.github.com>
2020-03-03 23:11:35 +0900

    Create Solution.cs

---
+++ HackerRank/30 Days of Code/Day 11 2D Arrays/Solution.cs
@@ -0,0 +1,42 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    static void Main(string[] args) {
+        int[][] arr = new int[6][];
+        int max = -64;
+        int sum = 0;
+
+        for (int i = 0; i < 6; i++) 
+        {
+            arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        }
+
+        for (int j = 0; j < 4; j++)
+        {
+            for (int k = 0; k < 4; k++)
+            {
+                sum = arr[j][k] + arr[j][k+1] + arr[j][k+2]
+                    + arr[j+1][k+1] 
+                    + arr[j+2][k] + arr[j+2][k+1] + arr[j+2][k+2];
+                
+                if (max < sum)
+                    max = sum;
+            }
+        }
+
+        Console.WriteLine(max);
+    }
+}
