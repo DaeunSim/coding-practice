9f93e10ce14fcad19212bd6719a49a89b30ea378
DaeunSim <49071747+DaeunSim@users.noreply.github.com>
2020-05-18 13:34:17 +0900

    Update Solution.cs
    Update solution

--- HackerRank/30 Days of Code/Day 14 Scope/Solution.cs
+++ HackerRank/30 Days of Code/Day 14 Scope/Solution.cs
@@ -14,28 +14,30 @@ class Difference {
 	// Add your code here
     public void computeDifference()
     {
-        int result = 0;
+        //int result = 0;
 
-        count = elements.Length;
-        for (int i = 0; i < count - 1; i++)
-        {
-            for (int j = i + 1; j < count; j++)
-            {
-                if (elements[i] <= elements[j])
-                {
-                    result = elements[j] - elements[i];
-                }
-                else
-                {
-                    result = elements[i] - elements[j];
-                }
-
-                if (maximumDifference < result)
-                {
-                    maximumDifference = result;
-                }
-            }
-        }
+        //count = elements.Length;
+        //for (int i = 0; i < count - 1; i++)
+        //{
+        //    for (int j = i + 1; j < count; j++)
+        //    {
+        //        if (elements[i] <= elements[j])
+        //        {
+        //            result = elements[j] - elements[i];
+        //        }
+        //        else
+        //        {
+        //            result = elements[i] - elements[j];
+        //        }
+        //   
+        //        if (maximumDifference < result)
+        //        {
+        //            maximumDifference = result;
+        //        }
+        //    }
+        //}
+	    
+	maximumDifference = elements.Max() - elements.Min();
     }
 
 } // End of Difference Class
