5dce3f72349211733a40ed2b880ba570499b145f
DaeunSim <49071747+DaeunSim@users.noreply.github.com>
2020-05-19 14:34:36 +0900

    Update Solution.cs

--- HackerRank/30 Days of Code/Day 13 Abstract Classes/Solution.cs
+++ HackerRank/30 Days of Code/Day 13 Abstract Classes/Solution.cs
@@ -1,19 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+
 abstract class Book
 {
-    
     protected String title;
     protected  String author;
     
-    public Book(String t,String a){
+    public Book(String t,String a)
+    {
         title=t;
         author=a;
     }
+    
     public abstract void display();
-
-
 }
 
 //Write MyBook class
@@ -34,12 +34,15 @@ class MyBook : Book
     }
 }
 
-class Solution {
-    static void Main(String[] args) {
-      String title=Console.ReadLine();
-      String author=Console.ReadLine();
-      int price=Int32.Parse(Console.ReadLine());
-      Book new_novel=new MyBook(title,author,price);
-      new_novel.display();
+class Solution 
+{
+    static void Main(String[] args) 
+    {
+        String title=Console.ReadLine();
+        String author=Console.ReadLine();
+        int price=Int32.Parse(Console.ReadLine());
+        
+        Book new_novel=new MyBook(title,author,price);
+        new_novel.display();
     }
 }
