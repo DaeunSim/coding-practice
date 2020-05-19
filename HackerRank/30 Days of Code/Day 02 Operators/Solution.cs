6b090d140fecb69eaf42f22ce4d9047cce3a1a44
DaeunSim <49071747+DaeunSim@users.noreply.github.com>
2020-05-19 14:28:53 +0900

    Update Solution.cs

--- HackerRank/30 Days of Code/Day 02 Operators/Solution.cs
+++ HackerRank/30 Days of Code/Day 02 Operators/Solution.cs
@@ -12,11 +12,11 @@ using System.Text.RegularExpressions;
 using System.Text;
 using System;
 
-class Solution {
-
+class Solution 
+{
     // Complete the solve function below.
-    static void solve(double meal_cost, int tip_percent, int tax_percent) {
-        
+    static void solve(double meal_cost, int tip_percent, int tax_percent) 
+    {      
         double tip = meal_cost * (tip_percent * 0.01);
         double tax = meal_cost * (tax_percent * 0.01);
 
@@ -29,11 +29,10 @@ class Solution {
         Console.Write(total);
     }
 
-    static void Main(string[] args) {
+    static void Main(string[] args) 
+    {
         double meal_cost = Convert.ToDouble(Console.ReadLine());
-
         int tip_percent = Convert.ToInt32(Console.ReadLine());
-
         int tax_percent = Convert.ToInt32(Console.ReadLine());
 
         solve(meal_cost, tip_percent, tax_percent);
