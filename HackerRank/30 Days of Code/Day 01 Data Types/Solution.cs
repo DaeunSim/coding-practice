6c14e1cb932c99948e0a30ecb7d61f887478ff3f
DaeunSim <49071747+DaeunSim@users.noreply.github.com>
2020-05-19 14:28:13 +0900

    Update Solution.cs

--- HackerRank/30 Days of Code/Day 01 Data Types/Solution.cs
+++ HackerRank/30 Days of Code/Day 01 Data Types/Solution.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 
-class Solution {
-    static void Main(String[] args) {
+class Solution 
+{
+    static void Main(String[] args) 
+    {
         int i = 4;
         double d = 4.0;
         string s = "HackerRank ";
