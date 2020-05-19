9e7ca222381bf7892149177660739d0a1ef6d748
DaeunSim <49071747+DaeunSim@users.noreply.github.com>
2020-05-19 14:30:47 +0900

    Update Solution.cs

--- HackerRank/30 Days of Code/Day 06 Let's Review/Solution.cs
+++ HackerRank/30 Days of Code/Day 06 Let's Review/Solution.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-class Solution {
-    static void Main(String[] args) {
+
+class Solution 
+{
+    static void Main(String[] args) 
+    {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
         int cases = Int32.Parse(Console.ReadLine());
 
