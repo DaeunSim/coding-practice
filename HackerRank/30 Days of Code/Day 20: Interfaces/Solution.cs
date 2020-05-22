edc00ebf2313d183d0d6b7a39dd780069340c2ae
DaeunSim <49071747+DaeunSim@users.noreply.github.com>
2020-05-22 11:48:59 +0900

    Create Solution.cs

---
+++ HackerRank/30 Days of Code/Day 20: Interfaces/Solution.cs
@@ -0,0 +1,37 @@
+using System;
+
+public interface AdvancedArithmetic
+{
+    int divisorSum(int n);
+}
+
+public class Calculator : AdvancedArithmetic
+{
+    public int divisorSum(int n)
+    {
+        if (n < 1 || n > 1000)
+            return 0;
+
+        int result = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                result += i;
+            }
+        }
+
+        return result;
+    }
+}
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = Int32.Parse(Console.ReadLine());
+      	AdvancedArithmetic myCalculator = new Calculator();
+        int sum = myCalculator.divisorSum(n);
+        Console.WriteLine("I implemented: AdvancedArithmetic\n" + sum); 
+    }
+}
