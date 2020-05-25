1938ff73ff3599ef064f1350644870ef7eb2544d
DaeunSim <49071747+DaeunSim@users.noreply.github.com>
2020-05-25 14:11:23 +0900

    Create Solution.cs

---
+++ HackerRank/30 Days of Code/Day 22 Binary Search Trees/Solution.cs
@@ -0,0 +1,76 @@
+sing System;
+
+class Node
+{
+    public Node left,right;
+    public int data;
+    public Node(int data)
+    {
+        this.data=data;
+        left=right=null;
+    }
+}
+
+class Solution
+{
+  // Not solved.
+  // Refer to discussions page
+	static int getHeight(Node root)
+  {
+      //Write your code here
+        int height = 0;
+
+        if (root == null)
+            return -1;
+        
+        int left = getHeight(root.left) + 1;
+        int right = getHeight(root.right) + 1;
+
+        if (left < right)
+        {
+            height = right;
+        }
+        else
+        {
+            height = left;
+        }
+
+        return height;
+    }
+
+	static Node insert(Node root, int data)
+  {
+        if(root==null)
+        {
+            return new Node(data);
+        }
+        else
+        {
+            Node cur;
+            if(data<=root.data)
+            {
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else
+            {
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    
+    static void Main(String[] args)
+    {
+        Node root=null;
+        int T=Int32.Parse(Console.ReadLine());
+        while(T-->0)
+        {
+            int data=Int32.Parse(Console.ReadLine());
+            root=insert(root,data);            
+        }
+        int height=getHeight(root);
+        Console.WriteLine(height);
+    }
+}
